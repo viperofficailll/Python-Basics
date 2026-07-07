a6a0852121893419cdc68d38ecca21a73cbccab0
viperofficialll <sharadhapokharel777@gmail.com>
2026-07-07 16:34:19 +0545

    data types done.

---
+++ data types/dataTypes.py
@@ -0,0 +1,16 @@
+name = "Sandesh" # string
+age = 22 # Integer
+cgpa = 3.85 #Float
+isintern = False  # Boolean
+skills = ["python" , "c#" ,"SQL"] # list
+coordinates = (28.2, 83.9)  # tuple
+student ={"name":"sandesh" ,"age":22} # dictionary
+
+
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(isintern))
+print(type(skills))
+print(type(coordinates))
+print(type(student))
